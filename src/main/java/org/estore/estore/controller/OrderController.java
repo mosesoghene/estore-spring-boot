670c88a54b211e0ede4411647b6c5c156e403683
Moses Oghene <mosesogheneo@gmail.com>
2025-05-23 15:46:33 +0100

    Order constroller

---
+++ src/main/java/org/estore/estore/controller/OrderController.java
@@ -0,0 +1,22 @@
+package org.estore.estore.controller;
+
+import lombok.AllArgsConstructor;
+import org.estore.estore.dto.request.CreateOrderRequest;
+import org.estore.estore.dto.response.CreateOrderResponse;
+import org.estore.estore.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@AllArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderRequest order) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(orderService.create(order));
+    }
+}
